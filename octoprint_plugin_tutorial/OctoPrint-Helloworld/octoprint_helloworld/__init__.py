182abf9b86446a6f14b4cb7474e030f215771b42
pattarsuraj <pattarsuraj@gmail.com>
2021-08-31 19:02:23 +0900

    :sparkles: Octoprint Plugin Tutorial

---
+++ octoprint_plugin_tutorial/OctoPrint-Helloworld/octoprint_helloworld/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import octoprint.plugin
+
+
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin):
+    def on_after_startup(self):
+        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+    def get_template_vars(self):
+        return dict(url=self._settings.get(["url"]))
+
+
+__plugin_name__ = "Hello World"
+__plugin_pythoncompat__ = ">=2.4,<4"
+__plugin_implementation__ = HelloWorldPlugin()
